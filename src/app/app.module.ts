1aeb32f8514d51a1a7d09060863617d6d755395f
PetrNikolas <elprokop7@gmail.com>
2017-04-02 13:06:10 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// Others libs
 import { Ng2PageTransitionModule } from "ng2-page-transition";
 
+// Routings
 import { routing } from './app.routes';
 
+// Components
 import { AppComponent } from './components/app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
