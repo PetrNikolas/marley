b4932262b5fb2c3372e91efc17db3e3ba2fe5be1
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2017-07-26 20:05:46 +0200

    update test db

--- api/config/test.exs
+++ api/config/test.exs
@@ -12,8 +12,6 @@ config :logger, level: :warn
 # Configure your database
 config :api, Api.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "api_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
