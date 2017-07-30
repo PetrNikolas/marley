574f2d92d1f519a4565eb5e4cdc02f9c317e80f4
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2017-07-30 13:13:57 +0200

    router bug fixed

--- api/web/router.ex
+++ api/web/router.ex
@@ -17,17 +17,11 @@ defmodule Api.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index, as: :root
-
-    get "/contact", ContactController, :index
   end
 
   # Other scopes may use custom stacks.
   scope "/api", Api do
     pipe_through :api
-    
-    #resources "/users", UserController, except: [:new, :edit]
-
-    #resources "/requests", RequestController, except: [:new, :edit]
 
     # Versions of API
     scope "/v1", V1, as: :v1 do
