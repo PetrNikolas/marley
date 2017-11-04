9bec872ccd4f78dc94aaca9584eca30f10f99820
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2017-11-04 09:33:51 +0100

    analyzer ready

--- README.md
+++ README.md
@@ -16,27 +16,63 @@ Mini Starter Kit for complex web apps.
 * [Sass](http://sass-lang.com/),
 * [Animate.css](https://github.com/daneden/animate.css).
 
-## Project Setup and start
+## Project Setup
 
-1. Run `npm install`.
-2. Run `npm run dev` and visit http://localhost:4200/. That's it!.
+1. Clone this repo.
+2. Run `npm install` to install dependencies.
 
-### For Development Server
-3. Run `npm run dev` and visit http://localhost:4200/.
+## Web Development
 
-### Code scaffolding
-4. Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive/pipe/service/class/module`.
+### Development build and serve with live reload
+1. Run `npm run serve` to build the project for development and serve.
+2. Visit http://localhost:4200/. That's it!.
 
-### Build for Production
-5. Run `npm run build:prod`
-6. Run `npm start` and visit http://localhost:8080/.
+### Build for development
 
-### Running unit tests
-7. Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+1. Run `npm run serve:dev` to build the project for development.
+2. Visit http://localhost:3000/.
 
-### Running end-to-end tests
-8. Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-9. Before running the tests make sure you are serving the app via `npm run dev`.
+### Build for production
 
-### Running lint
-10. Run `ng lint`.
+1. Run `npm run serve:prod` to build the project for production.
+2. Visit http://localhost:3000/.
+
+## Desktop Development
+
+### Desktop development build
+
+1. Run `npm run build:desktop`.
+2. Run `npm run serve:electron`.
+
+### Desktop production build
+
+#### For MacOS 
+1. Run `npm run package`.
+
+#### For Windows
+1. Run `npm run package:windows` or `package:windows:x64`.
+
+#### For Linux
+1. Run `npm run package:linux`.
+
+## Code scaffolding
+1. Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive/pipe/service/class/module`.
+
+## Running unit tests
+1. Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+
+## Running end-to-end tests
+1. Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
+2. Before running the tests make sure you are serving the app via `npm run dev`.
+
+## Lint
+
+1. Run `npm run lint`.
+
+## Analyze your bundle
+
+1. Run `npm run analyzer` and visit http://localhost:8888/.
+
+## Webpack dashboard
+
+1. Run `npm run webpack-dashboard` and visit webpack dashboard output in terminal.
--- tools/analyzer.js
+++ tools/analyzer.js
@@ -8,15 +8,6 @@ const path = require('path');
 //------------------------------------------------------------------------------
 // Import all plugins
 //------------------------------------------------------------------------------
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const OptimizeJsPlugin = require("optimize-js-plugin");
-const CompressionPlugin = require('compression-webpack-plugin');
-const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
-const WebpackCleanupPlugin = require('webpack-cleanup-plugin');
-const autoprefixer = require('autoprefixer');
-const poststylus = require('poststylus');
-const WebpackMd5Hash = require('webpack-md5-hash');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 
@@ -55,79 +46,10 @@ module.exports = {
     resolve: {
         extensions: [
             '.js'
-        ],
-        alias: {
-            'webworkify': 'webworkify-webpack',
-            'mapbox-gl': path.resolve('./node_modules/mapbox-gl/dist/mapbox-gl.js'),
-            '@mapbox/mapbox-gl-draw': path.resolve('./node_modules/@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.js')
-        }
+        ]
     },
     module: {
-        rules: [
-            { 
-                test: /\.(png|gif|jpg)$/, 
-                use: [
-                    { 
-                        loader: 'file-loader', 
-                        options: { name: 'images/[name].[ext]' } 
-                    }
-                ] 
-            },
-            { 
-                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, 
-                use: [
-                    { 
-                        loader: 'file-loader', 
-                        options: { name: 'fonts/[name].[ext]' } 
-                    }
-                ] 
-            },
-            { 
-                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, 
-                use: [
-                    { 
-                        loader: 'file-loader', 
-                        options: { name: 'fonts/[name].[ext]' } 
-                    }
-                ] 
-            },
-            { 
-                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
-                use: [
-                    { 
-                        loader: 'file-loader', 
-                        options: { name: 'fonts/[name].[ext]' } 
-                    }
-                ] 
-            },
-            { 
-                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
-                use: [
-                    { 
-                        loader: 'file-loader', 
-                        options: { name: 'fonts/[name].[ext]' } 
-                    }
-                ] 
-            },
-            { 
-                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
-                use: [
-                    { 
-                        loader: 'file-loader', 
-                        options: { name: 'fonts/[name].[ext]' } 
-                    }
-                ] 
-            },
-            {
-                enforce: 'post',
-                include: /node_modules\/mapbox-gl-shaders/,
-                loader: 'transform',
-                query: 'brfs'
-            }
-        ],
-        noParse: [
-            path.join(__dirname, 'node_modules', 'angular2', 'bundles')
-        ]
+        rules: []
     },
     plugins: [
         new BundleAnalyzerPlugin({
