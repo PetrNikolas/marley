139783e376761763647a187202c2cc8b4266ebcf
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2018-11-29 18:28:29 +0100

    remove version.js file

--- apps/landing-page/src/environments/version.ts
+++ apps/landing-page/src/environments/version.ts
@@ -1,14 +1,5 @@
-// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": true,
-    "raw": "f8d84fca-dirty",
-    "hash": "f8d84fca",
-    "distance": null,
-    "tag": null,
-    "semver": null,
-    "suffix": "f8d84fca-dirty",
-    "semverString": null,
     "version": "0.9.1"
 };
 /* tslint:enable */
